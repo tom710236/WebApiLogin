97dc977f6a0205fb5a22192880f173e7645672b8
TOM <tom710236@gmail.com>
2017-02-18 15:22:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tom/webapilogin/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.tom.webapilogin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    OkHttpClient client = new OkHttpClient();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Request request = new Request.Builder()
+                .url("http://demo.shinda.com.tw/ModernWebApi/WebApiLogin.aspx")
+                .build();
+        Call call =client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+                Log.d("OKHTTP", json);
+                parseJson(json);
+            }
+
+            private void parseJson(String json) {
+            }
+        });
+    }
+}
+
