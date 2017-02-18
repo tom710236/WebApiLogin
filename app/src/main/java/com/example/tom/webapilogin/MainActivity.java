d3e060c5f7fa8aa9f99a8f78aeac7a02cf775e22
TOM <tom710236@gmail.com>
2017-02-18 18:04:06 +0800

    解析JSON

--- app/src/main/java/com/example/tom/webapilogin/MainActivity.java
+++ app/src/main/java/com/example/tom/webapilogin/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -37,8 +40,21 @@ public class MainActivity extends AppCompatActivity {
             }
 
             private void parseJson(String json) {
+
+                try {
+                    String cMsg = new JSONObject(json).getString("cMsg");
+                    String cStatus = new JSONObject(json).getString("cStatus");
+                    String cUserID = new JSONObject(json).getString("cUserID");
+                    String cUserName = new JSONObject(json).getString("cUserName");
+                    Log.d("JSOM",cMsg);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
+
     }
 }
 
